2755a3734058603eaf9b8c1655b52715722dfa0a
John Michael Manlupig <manlupigjohnmichael@gmail.com>
2020-12-14 00:06:30 +0800

    Update

--- web/src/components/Theme.tsx
+++ web/src/components/Theme.tsx
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import state from 'src/services/state';
 
-export default class Theme extends Component {
+type Theme = 'dark' | 'light';
+
+interface State {
+	theme: Theme;
+}
+
+export default class ThemeComponent extends Component<{}, State> {
 	key = 0;
 
+	constructor(props: {}) {
+		super(props);
+		this.state = {
+			theme: state.get('theme'),
+		};
+	}
+
 	componentDidMount() {
-		this.key = state.listen<string>('theme', (theme) => {
-			console.log(theme);
+		this.key = state.listen<Theme>('theme', (theme) => {
+			this.setState({ theme });
 		});
 	}
 
